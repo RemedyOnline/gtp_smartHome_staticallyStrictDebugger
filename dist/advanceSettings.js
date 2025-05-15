87297ce9d0d6692d3a80e32a2905e45487ba9e85
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2025-05-15 14:54:11 +0000

    almost done

--- dist/advanceSettings.js
+++ dist/advanceSettings.js
@@ -1,219 +1,134 @@
 "use strict";
-var __awaiter =
-	(this && this.__awaiter) ||
-	function (thisArg, _arguments, P, generator) {
-		function adopt(value) {
-			return value instanceof P
-				? value
-				: new P(function (resolve) {
-						resolve(value);
-				  });
-		}
-		return new (P || (P = Promise))(function (resolve, reject) {
-			function fulfilled(value) {
-				try {
-					step(generator.next(value));
-				} catch (e) {
-					reject(e);
-				}
-			}
-			function rejected(value) {
-				try {
-					step(generator["throw"](value));
-				} catch (e) {
-					reject(e);
-				}
-			}
-			function step(result) {
-				result.done
-					? resolve(result.value)
-					: adopt(result.value).then(fulfilled, rejected);
-			}
-			step((generator = generator.apply(thisArg, _arguments || [])).next());
-		});
-	};
-var __classPrivateFieldGet =
-	(this && this.__classPrivateFieldGet) ||
-	function (receiver, state, kind, f) {
-		if (kind === "a" && !f)
-			throw new TypeError("Private accessor was defined without a getter");
-		if (
-			typeof state === "function"
-				? receiver !== state || !f
-				: !state.has(receiver)
-		)
-			throw new TypeError(
-				"Cannot read private member from an object whose class did not declare it"
-			);
-		return kind === "m"
-			? f
-			: kind === "a"
-			? f.call(receiver)
-			: f
-			? f.value
-			: state.get(receiver);
-	};
-var _AdvanceSettings_instances,
-	_AdvanceSettings_markup,
-	_AdvanceSettings_analyticsUsage;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _AdvanceSettings_instances, _AdvanceSettings_markup, _AdvanceSettings_analyticsUsage;
 // import { Chart } from "chart.js";
 import Light from "./basicSettings.js";
 class AdvanceSettings extends Light {
-	constructor() {
-		super();
-		_AdvanceSettings_instances.add(this);
-	}
-	modalPopUp(element) {
-		const selectedRoom = this.getSelectedComponentName(element);
-		const componentData = this.getComponent(selectedRoom);
-		const parentElement = this.selector(".advanced_features_container");
-		this.removeHidden(parentElement);
-		this.renderHTML(
-			__classPrivateFieldGet(
-				this,
-				_AdvanceSettings_instances,
-				"m",
-				_AdvanceSettings_markup
-			).call(this, componentData),
-			"afterbegin",
-			parentElement
-		);
-		__classPrivateFieldGet(
-			this,
-			_AdvanceSettings_instances,
-			"m",
-			_AdvanceSettings_analyticsUsage
-		).call(this, componentData["usage"]);
-	}
-	displayCustomization(selectedElement) {
-		const element = this.closestSelector(
-			selectedElement,
-			".customization",
-			".customization-details"
-		);
-		this.toggleHidden(element);
-	}
-	closeModalPopUp() {
-		const parentElement = this.selector(".advanced_features_container");
-		const childElement = this.selector(".advanced_features");
-		childElement.remove();
-		this.addHidden(parentElement);
-	}
-	customizationCancelled(selectedElement, parentSelectorIdentifier) {
-		const element = this.closestSelector(
-			selectedElement,
-			parentSelectorIdentifier,
-			"input"
-		);
-		element.value = "";
-		return;
-	}
-	customizeAutomaticOnPreset(selectedElement) {
-		const element = this.closestSelector(
-			selectedElement,
-			".defaultOn",
-			"input"
-		);
-		const { value } = element;
-		if (!value) return;
-		const component = this.getComponentData(
-			element,
-			".advanced_features",
-			".component_name"
-		);
-		component.autoOn = value;
-		element.value = "";
-		const spanElement = this.selector(".auto_on > span:last-child");
-		this.updateMarkupValue(spanElement, component.autoOn);
-		this.setComponentElement(component);
-		this.automateLight(component["autoOn"], component);
-	}
-	customizeAutomaticOffPreset(selectedElement) {
-		const element = this.closestSelector(
-			selectedElement,
-			".defaultOff",
-			"input"
-		);
-		const { value } = element;
-		if (!value) return;
-		const component = this.getComponentData(
-			element,
-			".advanced_features",
-			".component_name"
-		);
-		component.autoOff = value;
-		element.value = "";
-		const spanElement = this.selector(".auto_off > span:last-child");
-		this.updateMarkupValue(spanElement, component.autoOff);
-		this.setComponentElement(component);
-		this.automateLight(component["autoOff"], component.autoOff);
-	}
-	getSelectedComponent(componentName) {
-		if (!componentName) return this.componentsData[0];
-		const component = this.componentsData[componentName.toLowerCase()];
-		return component;
-	}
-	getSelectedSettings(componentName) {
-		return __classPrivateFieldGet(
-			this,
-			_AdvanceSettings_instances,
-			"m",
-			_AdvanceSettings_markup
-		).call(this, this.getSelectedComponent(componentName));
-	}
-	setNewData(component, key, data) {
-		const selectedComponent = this.componentsData[component.name];
-		return (selectedComponent[key] = data);
-	}
-	capFirstLetter(word) {
-		return word.replace(word[0], word[0].toUpperCase());
-	}
-	getObjectDetails() {
-		return this;
-	}
-	formatTime(time) {
-		const [hour, min] = time.split(":");
-		const dailyAlarmTime = new Date();
-		dailyAlarmTime.setHours(parseInt(hour));
-		dailyAlarmTime.setMinutes(parseInt(min));
-		dailyAlarmTime.setSeconds(0);
-		return dailyAlarmTime;
-	}
-	timeDifference(selectedTime) {
-		const now = new Date();
-		const setTime = this.formatTime(selectedTime).getTime() - now.getTime();
-		console.log(setTime, now);
-		return setTime;
-	}
-	timer(time, message, component) {
-		return __awaiter(this, void 0, void 0, function* () {
-			return new Promise((resolve, reject) => {
-				const checkAndTriggerAlarm = () => {
-					const now = new Date();
-					if (
-						now.getHours() === time.getHours() &&
-						now.getMinutes() === time.getMinutes() &&
-						now.getSeconds() === time.getSeconds()
-					) {
-						resolve(this.toggleLightSwitch(component));
-						clearInterval(intervalId);
-					}
-				};
-				const intervalId = setInterval(checkAndTriggerAlarm, 1000);
-			});
-		});
-	}
-	automateLight(time, component) {
-		return __awaiter(this, void 0, void 0, function* () {
-			const formattedTime = this.formatTime(time);
-			return yield this.timer(formattedTime, true, component);
-		});
-	}
+    constructor() {
+        super();
+        _AdvanceSettings_instances.add(this);
+    }
+    modalPopUp(element) {
+        const selectedRoom = this.getSelectedComponentName(element);
+        const componentData = this.getComponent(selectedRoom);
+        const parentElement = this.selector(".advanced_features_container");
+        this.removeHidden(parentElement);
+        this.renderHTML(__classPrivateFieldGet(this, _AdvanceSettings_instances, "m", _AdvanceSettings_markup).call(this, componentData), "afterbegin", parentElement);
+        __classPrivateFieldGet(this, _AdvanceSettings_instances, "m", _AdvanceSettings_analyticsUsage).call(this, componentData["usage"]);
+    }
+    displayCustomization(selectedElement) {
+        const element = this.closestSelector(selectedElement, ".customization", ".customization-details");
+        this.toggleHidden(element);
+    }
+    closeModalPopUp() {
+        const parentElement = this.selector(".advanced_features_container");
+        const childElement = this.selector(".advanced_features");
+        childElement.remove();
+        this.addHidden(parentElement);
+    }
+    customizationCancelled(selectedElement, parentSelectorIdentifier) {
+        const element = this.closestSelector(selectedElement, parentSelectorIdentifier, "input");
+        element.value = "";
+        return;
+    }
+    customizeAutomaticOnPreset(selectedElement) {
+        const element = this.closestSelector(selectedElement, ".defaultOn", "input");
+        const { value } = element;
+        if (!value)
+            return;
+        const component = this.getComponentData(element, ".advanced_features", ".component_name");
+        component.autoOn = value;
+        element.value = "";
+        const spanElement = this.selector(".auto_on > span:last-child");
+        this.updateMarkupValue(spanElement, component.autoOn);
+        this.setComponentElement(component);
+        this.automateLight(component["autoOn"], component);
+    }
+    customizeAutomaticOffPreset(selectedElement) {
+        const element = this.closestSelector(selectedElement, ".defaultOff", "input");
+        const { value } = element;
+        if (!value)
+            return;
+        const component = this.getComponentData(element, ".advanced_features", ".component_name");
+        component.autoOff = value;
+        element.value = "";
+        const spanElement = this.selector(".auto_off > span:last-child");
+        this.updateMarkupValue(spanElement, component.autoOff);
+        this.setComponentElement(component);
+        this.automateLight(component["autoOff"], component.autoOff);
+    }
+    getSelectedComponent(componentName) {
+        if (!componentName)
+            return this.componentsData[0];
+        const component = this.componentsData[componentName.toLowerCase()];
+        return component;
+    }
+    getSelectedSettings(componentName) {
+        return __classPrivateFieldGet(this, _AdvanceSettings_instances, "m", _AdvanceSettings_markup).call(this, this.getSelectedComponent(componentName));
+    }
+    setNewData(component, key, data) {
+        const selectedComponent = this.componentsData[component.name];
+        return (selectedComponent[key] = data);
+    }
+    capFirstLetter(word) {
+        return word.replace(word[0], word[0].toUpperCase());
+    }
+    getObjectDetails() {
+        return this;
+    }
+    formatTime(time) {
+        const [hour, min] = time.split(":");
+        const dailyAlarmTime = new Date();
+        dailyAlarmTime.setHours(parseInt(hour));
+        dailyAlarmTime.setMinutes(parseInt(min));
+        dailyAlarmTime.setSeconds(0);
+        return dailyAlarmTime;
+    }
+    timeDifference(selectedTime) {
+        const now = new Date();
+        const setTime = this.formatTime(selectedTime).getTime() - now.getTime();
+        console.log(setTime, now);
+        return setTime;
+    }
+    timer(time, message, component) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return new Promise((resolve, reject) => {
+                const checkAndTriggerAlarm = () => {
+                    const now = new Date();
+                    if (now.getHours() === time.getHours() &&
+                        now.getMinutes() === time.getMinutes() &&
+                        now.getSeconds() === time.getSeconds()) {
+                        resolve(this.toggleLightSwitch(component));
+                        clearInterval(intervalId);
+                    }
+                };
+                const intervalId = setInterval(checkAndTriggerAlarm, 1000);
+            });
+        });
+    }
+    automateLight(time, component) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const formattedTime = this.formatTime(time);
+            return yield this.timer(formattedTime, true, component);
+        });
+    }
 }
-(_AdvanceSettings_instances = new WeakSet()),
-	(_AdvanceSettings_markup = function _AdvanceSettings_markup(component) {
-		const { name, numOfLights, autoOn, autoOff } = component;
-		return `
+_AdvanceSettings_instances = new WeakSet(), _AdvanceSettings_markup = function _AdvanceSettings_markup(component) {
+    const { name, numOfLights, autoOn, autoOff } = component;
+    return `
         <div class="advanced_features">
             <h3>Advanced features</h3>
             <section class="component_summary">
@@ -277,30 +192,27 @@ class AdvanceSettings extends Light {
             </button>
         </div>
         `;
-	}),
-	(_AdvanceSettings_analyticsUsage = function _AdvanceSettings_analyticsUsage(
-		data
-	) {
-		const ctx = this.selector("#myChart");
-		new Chart(ctx, {
-			type: "line",
-			data: {
-				labels: ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"],
-				datasets: [
-					{
-						label: "Hours of usage",
-						data: data,
-						borderWidth: 1,
-					},
-				],
-			},
-			options: {
-				scales: {
-					y: {
-						beginAtZero: true,
-					},
-				},
-			},
-		});
-	});
+}, _AdvanceSettings_analyticsUsage = function _AdvanceSettings_analyticsUsage(data) {
+    const ctx = this.selector("#myChart");
+    new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"],
+            datasets: [
+                {
+                    label: "Hours of usage",
+                    data: data,
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+};
 export default AdvanceSettings;
